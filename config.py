c21143540ff25180bd5aa82be65c7c7715ae6877
Auliaa-06 <auliaazarah22@gmail.com>
2026-06-26 02:13:28 +0700

    bypass ssl untuk vercel cloud

--- config.py
+++ config.py
@@ -3,14 +3,12 @@ import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-# Gunakan string koneksi khusus Connection Pooling dari Supabase (Port 6543)
-# Ini wajib untuk platform serverless seperti Vercel biar koneksi tidak putus-putus
-DATABASE_URL = "postgresql://postgres:bZHVYNNibi5r9Mie@db.qwokowmwcfldqwjxpbfs.supabase.co:6543/postgres?sslmode=require"
+# sslmode diganti menjadi disable agar bypass verifikasi sertifikat di server cloud Vercel
+DATABASE_URL = "postgresql://postgres:bZHVYNNibi5r9Mie@db.qwokowmwcfldqwjxpbfs.supabase.co:6543/postgres?sslmode=disable"
 
 def get_db_connection():
     try:
-        # Menambahkan parameter connect_timeout agar Vercel tidak menggantung lama jika gagal
-        conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor, connect_timeout=5)
+        conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor, connect_timeout=10)
         return conn
     except Exception as e:
         print(f"Gagal konek ke database cloud: {e}")
